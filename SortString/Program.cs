a30108c538aa2727812b6399d11a558459445376
MenesiZoltan <colos.menesi@gmail.com>
2019-04-26 13:40:43 +0200

    Solved Sort Excercises

---
+++ SortString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "My name is Luballo Lubido lulugan of the Lulen people.";
+            string[] words = testString.Split(' ');
+
+            ArrayPrinter(words);
+
+            Array.Sort(words);
+
+            ArrayPrinter(words);
+
+            string s = string.Join(" ", words);
+
+            Console.WriteLine("\n" + s);
+
+            Console.ReadLine();
+
+            
+        }
+
+        public static void ArrayPrinter(string[] words) {
+
+            Console.WriteLine("\n");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+           
+
+
+        }
+    }
+}
